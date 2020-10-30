d5698132bdb5d797d07114f9c64dd2df3fad7bb9
Dristi123 <queendristi27@gmail.com>
2020-10-30 22:21:59 +0600

    stash by dristi

--- Ishtishon/Ishtishon/urls.py
+++ Ishtishon/Ishtishon/urls.py
@@ -4,8 +4,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
+<<<<<<< Updated upstream
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home') #test
+=======
+    1. Add an import:  from my_app import views 7 no line change
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+>>>>>>> Stashed changes
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
