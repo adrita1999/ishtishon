424d6436b4abfe2bb0db68b6f670ecc67b71075d
adrita1999 <1705019@ugrad.cse.buet.ac.bd>
2020-11-30 20:51:12 +0600

    change mail page updated by nakshi

--- Ishtishon/rs/views.py
+++ Ishtishon/rs/views.py
@@ -683,6 +683,10 @@ def changemail(request):
     slice_object = slice(4, 14, 1)
     pnr = contact[slice_object]
     request.session["pnr"] = pnr
+
+    #btn1 first submit button jeta input nibe
+    #btn3 second submit button jeta otp ar password input nibe
+
     return render(request, 'changemail.html',{"fullname":fullname,"mail":mail,"address":address,"contact":contact,"pnr":pnr,"nid":nid})
 
 def changenum(request):
